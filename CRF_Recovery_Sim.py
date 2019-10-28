05836b21832a7ee93c37558f26b07fda22699235
Keegan Crankshaw <kcranky@gmail.com>
2019-10-28 10:40:20 +0200

    Initial work, including output plot. Evident logic errors

--- CRF_Recovery_Sim.py
+++ CRF_Recovery_Sim.py
@@ -10,9 +10,13 @@ Assumptions:
 """
 
 from generate_data import generate_timestamps as get_timestamps
+import matplotlib.pyplot as plt
+import datetime
 
 
-def reconstruct(duration, frequency):
+def reconstruct(frequency, duration):
+    toggle_ts = []
+    out_arr = []
     print("generating timestamps")
     timestamps = get_timestamps(frequency, duration)
     threshold = round(frequency/2,0)
@@ -27,14 +31,30 @@ def reconstruct(duration, frequency):
         if (gPTP >= ts) and (difference < threshold):
             if out == 0:
                 print("Toggling to high at gPTP {}".format(gPTP))
+                toggle_ts.append(gPTP)
+                out_arr.append(1)
             out = 1
+
         else:
             if out == 1:
                 print("Toggling to low at gPTP {}".format(gPTP))
+                toggle_ts.append(gPTP)
+                out_arr.append(0)
             out = 0
+
         if (ts < gPTP) and (out == 0):
             ts_index += 1
             ts = timestamps[ts_index]
         # Move to next nanosecond
         gPTP += 1
+    plot_square_wave(toggle_ts, out_arr)
     pass
+
+
+def plot_square_wave(x_axis, y_axis):
+    plt.plot(x_axis, y_axis, marker='d', color='blue', drawstyle='steps-pre')
+    plt.title("Waveform")
+    plt.ylabel('Amplitude')
+    plt.xlabel("Time")
+    plt.savefig("waveform{:%d%m%Y%H%M}.png".format(datetime.datetime.now()))
+    plt.show()
