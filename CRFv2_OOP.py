57e167b2a731d8ea8502f5bab71a35683c5c87da
Keegan Crankshaw <kcranky@gmail.com>
2020-02-14 09:34:26 +0200

    more work on OOP impl, some complications with the scaling from cs2k

--- CRFv2_OOP.py
+++ CRFv2_OOP.py
@@ -44,14 +44,18 @@ class SourceMClk:
 
 # Module responsible for creating the output/control wave to the CS2000
 class CSGen:
+
     threshold = 1302
 
     def __init__(self):
         self.state = 0
-        self.count_to = 12499  # the value the CSGen must count to
+        #self.count_to = 12499  # the value the CSGen must count to
+        self.count_to = 500  # the value the CSGen must count to
         self.local_count = 0
         self.local_count_scale = 1  # How many times slower is the CS2000 driver module than the gPTP module?
         self.generated_timestamp = 0  # The most recent local timestamp
+
+        self.clkdiv = CLKDIV() # probably not the best place to put this?
 
     def ocw_control(self, gptp_time):
         # increase local count by a related amount
@@ -62,6 +66,8 @@ class CSGen:
         if self.local_count == self.count_to:
             print("{} - Toggling OCW".format(gptp_time))
             self.local_count = 0
+            if self.state == 0:
+                self.clkdiv.activate(gptp_time)
             self.state = not self.state
 
             # if it's a rising edge, we need to call the clk_div
@@ -86,20 +92,33 @@ class CSGen:
 
 # Takes the CS2000 OCW, multiplies it up a bunch, and gives us a 48khz out
 # This STILL NEEDS TO DEPEND ON GPTP TIME! Perhaps an "is active" flag?
-class CLK_DIV:
+class CLKDIV:
     multiplier = 24756
     output_freq = 48000
     output_period = 1 / output_freq * pow(10, 9)  # convert to nS
 
-
+    # TODO: Look at the maths behind the CS2k module that was doumented in
+    # The output wave is given by 24576*source wave
+    # This "interim wave" is the divided down by 512 to generate a 48kHz wave
     def __init__(self):
-        pass
+        self.active = False
+        self.last_trigger = 0
 
-    def activate(self, g):
-        for i in range(multiplier+1):
-            if i % output_period == 0:
+    def activate(self, gptp_time):
+        self.active = True
+        # we need to determine the rate at which the "activate" signals come in,
+        # in order to multiply up and determine when to trigger
+        difference = gptp_time - self.last_trigger
+        self.last_trigger = gptp_time
+        rate = (difference) * 24576 # this gives us how often we toggle the "iterim" wave
+        mclk_rate = rate/512
+        print("time={}; diff={}; rate={}; mclk_rate={}".format(gptp_time, difference, rate, mclk_rate))
+
+    def trigger(self, g):
+        if self.active:
+            pass
 
 
 if __name__ == '__main__':
     sim = GPTPGenerator()
-    sim.run(99999)
+    sim.run(999999*2)
