d2cc1c9fba288bc4d21af49cd7e7eb1aee4f84ef
Keegan Crankshaw <kcranky@gmail.com>
2020-02-24 12:34:16 +0200

    Added a file for future work

---
+++ clock_recovery_algos.py
@@ -0,0 +1,17 @@
+"""
+This file should contain various potential recovery algorithms for testing, validation and comparison.
+"""
+
+
+def rev1():
+    """
+
+    :return:
+    """
+    return
+
+
+def rev2():
+    return
+
+# etc
